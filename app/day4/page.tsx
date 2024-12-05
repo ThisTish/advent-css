6c10b27281ff5e8534a513d6b048e4848db8e8b3
Tish <tishthegoat@gmail.com>
2024-12-05 13:02:24 -0700

    header

---
+++ app/day4/page.tsx
@@ -0,0 +1,17 @@
+const DayFourPage = () => {
+	return (
+		<>
+		<h1>Day Four</h1>
+		<div className="flex justify-center items-center m-10">
+			<hr className="w-1/3 mx-10 border-2 border-emerald-600"/>
+			<h2 className="text-5xl text-nowrap text-rose-900">
+				Merry Christmas
+			</h2>
+			<hr className="w-1/3 mx-10 border-2 border-emerald-600"/>
+
+		</div>
+		</>
+	)
+}
+
+export default DayFourPage
