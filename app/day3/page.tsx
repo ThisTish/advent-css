2313ed2529342c02c0e6ac15b9e5410464793da3
Tish <tishthegoat@gmail.com>
2024-12-05 12:34:32 -0700

    tool tip

---
+++ app/day3/page.tsx
@@ -0,0 +1,13 @@
+import styles from './day3.module.css'
+
+
+const DayThreePage = () => {
+	return (
+		<div>
+			<h1>Day Three</h1>
+			<h2 className= {styles["tooltip"]} data-tooltip={'Merry Xmas'}>Happy Holidays</h2>
+		</div>
+	)
+}
+
+export default DayThreePage
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
+
+
 
 export default function Home() {
   return (
     <div className="">
+      
     
     </div>
   );
