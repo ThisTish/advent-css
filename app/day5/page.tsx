f373c9d2e17d3890e4dda6f659af61bea427db71
Tish <tishthegoat@gmail.com>
2024-12-09 09:43:01 -0500

    footer day 5

---
+++ app/day5/page.tsx
@@ -0,0 +1,15 @@
+const DayFivePage = () => {
+	return (
+		<div>
+			<h1>Day Five</h1>
+			<main>
+				<p className="min-h-dvh">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis consectetur ipsam error illo! Commodi ea assumenda similique maiores, sequi odio laudantium laborum provident quasi veritatis dolore minima, quisquam officiis dolores. Fugiat at repellendus velit quo totam maxime, recusandae possimus ex voluptatibus libero eius incidunt molestiae culpa, consequuntur fuga explicabo accusamus?</p>
+			</main>
+			<footer className="sticky bottom-0 bg-emerald-900 w-full h-20 flex items-center ">
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, at.</p>
+			</footer>
+		</div>
+	)
+}
+
+export default DayFivePage
